f02c4cae1258bc75d99bfe1ad84869c423edcad1
Oscar Ramirez <oramirezent@gmail.com>
2024-08-12 17:52:38 -0400

    8/12

--- week-3/day-1/TSDemo/basics.ts
+++ week-3/day-1/TSDemo/basics.ts
@@ -107,16 +107,6 @@ enum zipCode {
 // console.log(zipCode.LosAngeles);
 
 function isLeapYear(year: number): boolean {
-    // if (year % 4 === 0) {
-    //     if (year % 100 === 0) {
-    //         if (year % 400 === 0) {
-    //             return true;
-    //         }
-    //         return false;
-    //     }
-    //     return true;
-    // }
-    // return false;
     return (year % 4 === 0) && ((year % 100 !== 0) || year % 400 === 0);
 }
 
